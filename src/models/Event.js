c63e681daf69ab21499d3a7c3a19e1fcbcabe9a7
Damola Adewunmi <damolasd@gmail.com>
2020-05-20 23:49:37 +0100

    added subdocuments

--- src/models/Event.js
+++ src/models/Event.js
@@ -1,6 +1,43 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const updateSchema = new Schema(
+  {
+    message: {
+      type: String,
+      required: true
+    }, 
+    image: {
+      type: String
+    }, 
+    datePosted: {
+      type: Date
+    }
+  }
+);
+
+const attendeeSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  phoneNumber: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  address: {
+    type: String
+  }
+});
+
 const eventSchema = new Schema({
   title: {
     type: String,
@@ -32,4 +69,8 @@ const eventSchema = new Schema({
       type: String,
     }
   ],
+  attendees: [ attendeeSchema ],
+  updates: [ updateSchema ]
 });
+
+export const Event = mongoose.model('Event', eventSchema);
---
+++ src/models/Feedback.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const feedbackSchema = new Schema({
+    email: {
+        type: String,
+        required: true
+    }, 
+    description: {
+        type: String
+    }
+});
+ export const Feedback = mongoose.model('Feedback', feedbackSchema);
