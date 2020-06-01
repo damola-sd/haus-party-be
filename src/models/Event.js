73018ae9578f8a088c7236956a823529f884ca51
Damola Adewunmi <damolasd@gmail.com>
2020-06-01 22:19:49 +0100

    city, state and country added

--- src/models/Event.js
+++ src/models/Event.js
@@ -50,6 +50,18 @@ const eventSchema = new Schema({
     type: String,
     required: true,
   },
+  city: {
+    type: String,
+    required: true
+  }, 
+  state: {
+    type: String,
+    required: true
+  },
+  country: {
+    type: String,
+    required: true
+  },
   latitude: {
     type: String,
   },
