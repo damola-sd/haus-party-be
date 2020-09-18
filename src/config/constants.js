839baf4f01c38f947310f0e888568fa37ba3fa3d
Damola Adewunmi <damolasd@gmail.com>
2020-09-18 16:58:26 +0100

    Aded some env variables

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,5 +1,5 @@
 import dotenv from 'dotenv';
 dotenv.config();
 export const {
-    MODE, DEV_DB_URL, TEST_DB_URL, STAGE_DB_URL, PROD_DB_URL
+    MODE, DEV_DB_URL, TEST_DB_URL, STAGE_DB_URL, PROD_DB_URL, TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, VERIFICATION_SID
 } = process.env;
