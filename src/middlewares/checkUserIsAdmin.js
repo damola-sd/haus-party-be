8afa29622e9afcee3844e4a535f5c99ea7b04d2c
Damola Adewunmi <damolasd@gmail.com>
2020-09-18 15:47:52 +0100

    added middleware to authenticate user

---
+++ src/middlewares/checkUserIsAdmin.js
@@ -0,0 +1,25 @@
+import User from '../models/User';
+
+
+const checkUserIsAdmin = async ({ decoded: { username } }, res, next) => {
+  try {
+    const userFound = await User.findByUsername(username);
+
+    if (!userFound) {
+      return res.status(404).json({
+        message: 'You do not seem to be registered, please sign up or try again',// eslint-disable-line
+      });
+    }
+
+    if (userFound.role !== 'admin') {
+      return res.status(403).json({
+        message: 'You are not authorized to perform this action',
+      });
+    }
+    next();
+  } catch (error) {
+    return res.status(500).json({ message: "An error occurred", error });
+  }
+};
+
+export default checkUserIsAdmin;
