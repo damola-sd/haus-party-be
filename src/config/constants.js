143dbbf1a08f6c4344858090f793958ddb250e5e
Damola Adewunmi <damolasd@gmail.com>
2020-09-23 01:12:15 +0100

    made changes

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,5 +1,13 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 export const {
-    MODE, DEV_DB_URL, TEST_DB_URL, STAGE_DB_URL, PROD_DB_URL, TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, VERIFICATION_SID
+  MODE,
+  DEV_DB_URL,
+  TEST_DB_URL,
+  STAGE_DB_URL,
+  PROD_DB_URL,
+  TWILIO_ACCOUNT_SID,
+  TWILIO_AUTH_TOKEN,
+  VERIFICATION_SID,
+  STRIPE_SECRET_KEY,
 } = process.env;
