448cea77ac55abf8e8a4f11b81384424da878b2a
Damola Adewunmi <damolasd@gmail.com>
2020-09-23 01:11:33 +0100

    added events controller

--- src/controllers/event.js
+++ src/controllers/event.js
@@ -1,6 +1,7 @@
 const dbQuery = require('../utils/dbQueries');
 const response = require('../utils/response');
-const Event = require('../models/Event');
+const locationQuery = require('../utils/locationQuery');
+const event = require('../models/Event');
 const NodeGeoCoder = require('node-geocoder');
 
 const options = {
@@ -12,49 +13,125 @@ const options = {
 };
 let geocoder = NodeGeoCoder(options);
 
-export const createEvent = async (req, res) => {
-    try {
-        const event = {
-            title: req.body.title,
-            description: req.body.description,
-            address: req.body.address,
-            city: req.body.city,
-            state: req.body.state,
-            country: req.body.country,
-            date: req.body.date,
-            time: req.body.time,
-            dressCode: req.body.dressCode,
-            price: req.body.price,
-            maxAttendees: req.body.maxAttendees
-        };
-        const address = `${req.body.address}, ${req.body.city}, ${req.body.state}, ${req.body.country}`
-        const newEvent = await dbQuery.Create(Event, event);
-        if (newEvent) {
+/**
+ * @class Event
+ */
+class Event {
+     /**
+   * @static
+   * @param {object} req
+   * @param {object} res
+   * @returns {json} response
+   * @memberof Event
+   */
+    static async createEvent(req, res) {
+        try {
+            const { title, description, date, time, dressCode, price, maxAttendees, address, city, country } = req.body;
+    
+            const newEvent = {
+                title,
+                description,
+                location: {
+                    address,
+                    city,
+                    country,
+                },
+                date,
+                time,
+                dressCode,
+                price,
+                maxAttendees
+            };
+            const address = `${req.body.address}, ${req.body.city}, ${req.body.country}`;
             const locationData = geocoder.geocode(address);
-            newEvent.latitude = locationData.latitude;
-            newEvent.longitude = locationData.longitude;
-            newEvent.save().then((event, error) => {
-                if (error) response.error(500, `Error: ${error.message}`)
-                response.success(res, 200, event);
+            const createdEvent = await dbQuery.Create(event, newEvent);
+            if (createdEvent) {
+                createdEvent.location.latitude = locationData.latitude;
+                createdEvent.location.longitude = locationData.longitude;
+                createdEvent.save().then((event, error) => {
+                    if (error) response.error(500, `Error: ${error.message}`)
+                    response.success(res, 200, event);
+                })
+            }
+            res.status(401).json({message: 'Could not create event'});
+    
+        } catch (error) {
+            throw error
+        }
+    }
+    
+     /**
+   * @static
+   * @param {object} req
+   * @param {object} res
+   * @returns {json} response
+   * @memberof Event
+   */
+    static async getEvents(req, res) {
+        const events = await Event.find({});
+        if (events) {
+            return response.success(res, 200, events);
+        }
+    }
+    
+     /**
+   * @static
+   * @param {object} req
+   * @param {object} res
+   * @returns {json} response
+   * @memberof Event
+   */
+    static async getEvent (req, res) {
+        const event = await Event.findById(req.params.id);
+        if (!event) {
+            response.error(res, 400, 'Event does not exist')
+        }
+        response.success(res, 200, event);
+    }
+    
+     /**
+   * @static
+   * @param {object} req
+   * @param {object} res
+   * @returns {json} response
+   * @memberof Event
+   */
+    static async getEventByCity (req, res) {
+    
+        const events = await Event.find()
+    }
+    
+     /**
+   * @static
+   * @param {object} req
+   * @param {object} res
+   * @returns {json} response
+   * @memberof Event
+   */
+
+    static async getNearByEvents (req, res) {
+        try {
+            const {
+                longitude, lattitude, page, limit,
+              } = req.query;
+              const nearByEvents = await event.find({locationQuery(longitude, lattitude) });
+              if (nearByEvents.length > 0) {
+                  return response.status(200).json({
+                    message: 'Found nearby events', 
+                    nearByEvents 
+                  });
+              }
+              response.status(400).json({
+                  message: 'There are no events in the area'
+              })
+
+        } catch (error) {
+            return res.status(500).json({
+                message: error.message 
             })
         }
 
-    } catch (error) {
-        throw error
     }
-}
 
-export const getEvents = async (req, res) => {
-    const events = await Event.find({});
-    if (events) {
-        return response.success(res, 200, events);
-    }
-}
-
-export const getEvent = async (req, res) => {
-    const event = await Event.findById(req.params.id);
-    if (!event) {
-        response.error(res, 400, 'Event does not exist')
-    }
-    response.success(res, 200, event);
+    
 }
