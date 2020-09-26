94169094535310f2fe6002bcc9ae2acb8738082e
Damola Adewunmi <damolasd@gmail.com>
2020-09-26 06:21:34 +0100

    added new card controller

--- src/controllers/card.js
+++ src/controllers/card.js
@@ -39,23 +39,58 @@ class Card {
    * @param {object} req
    * @param {object} res
    * @returns {object} response data
-   * @memberof User class
+   * @memberof Card class
    */
   static async makeCardDefaultCard(req, res) {
-      try{
-        const { id } = req.params;
-        const checkCard = await card.findById(id);
-        if(!checkCard) {
-          res.status(404).json({message: "Could not find the card"}) ;
-        }
-        const userInfo = await user.findByUsername(req.decoded.username);
-        userInfo.defaultCard = checkCard._id
-        userInfo.save().then(response => res.status(201).json({message: 'Successfully changed default card'}));
-
-
-      } catch (error) {
-        return res.status(500).json({message: err.message });
+    try {
+      const { id } = req.params;
+      const checkCard = await card.findById(id);
+      if (!checkCard) {
+        res.status(404).json({ message: "Could not find the card" });
       }
+      const userInfo = await user.findByUsername(req.decoded.username);
+      if (userInfo._id === checkCard.owner) {
+        userInfo.defaultCard = checkCard._id;
+        userInfo
+          .save()
+          .then((response) =>
+            res
+              .status(201)
+              .json({ message: "Successfully changed default card" })
+          );
+      }
+      return res.status(403).json({
+        message: "Card doesn't belong to you. Access denied",
+      });
+    } catch (error) {
+      return res.status(500).json({ message: err.message });
+    }
+  }
 
+  /**
+   *
+   * @param {object} req
+   * @param {object} res
+   * @returns {object} response data
+   * @memberof Card class
+   */
+
+  static async fetchAllUserCard({ decoded: { id, username } }, res) {
+    try {
+      const allUserCards = await card.find({ owner: id });
+      if (allUserCards) {
+        return res.status(200).json({
+          message: `Fetched all cards belonging to ${username}`,
+          data: allUserCards,
+        });
+      }
+      return res.status(204).json({
+        message: "No cards belonging to this user",
+      });
+    } catch (error) {
+      res.status(500).json({
+        message: error.message
+      })
+    }
   }
 }
