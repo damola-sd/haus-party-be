ddb8d65a366f7a3d64b0055b2c2ddf26d2e6afec
Damola Adewunmi <damolasd@gmail.com>
2021-03-03 03:32:40 +0100

    made changes

--- src/controllers/stripe.js
+++ src/controllers/stripe.js
@@ -1,11 +1,2 @@
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
-const getPaymentSecret = (req, res, next) => {
-  const amount = req.body.amount;
-  const currency = req.body.currency;
-
-  stripe.paymentIntents
-    .create({ amount, currency })
-    .then((intent) => res.json({ secret: intent["client_secret"] }))
-    .catch(next);
-};
--- src/controllers/transaction.js
+++ src/controllers/transaction.js
@@ -4,7 +4,20 @@ const stripe = new Stripe(STRIPE_SECRET_KEY);
 
 class Transaction {
     
-    static async charge(req, res) {
 
+
+
+    static async getPaymentSecret (req, res, next) {
+        const amount = req.body.amount;
+        const currency = req.body.currency;
+      
+        stripe.paymentIntents
+          .create({ amount, currency })
+          .then((intent) => res.json({ secret: intent["client_secret"] }))
+          .catch(next);
+      };
+      
+    static async charge(req, res) {
+        
     }
 }
